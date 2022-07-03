7db7d687f153a917b849bc6b63e0021e1ddf0126
rulewrite <rulewrite@gmail.com>
2022-07-03 15:46:44 +0900

    feat: 파일 읽기 기능 추가

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +9,7 @@ fn main() {
 
     println!("Searching for {:?}", query);
     println!("In file {:?}", filename);
+
+    let contents = fs::read_to_string(filename).expect("파일을 읽는 동안 문제가 발생했습니다.");
+    println!("With text:\n{}", contents);
 }
