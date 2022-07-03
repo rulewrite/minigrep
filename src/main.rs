46878d405845597bef780151e07672f9f9153be8
rulewrite <rulewrite@gmail.com>
2022-07-03 18:31:09 +0900

    feat: 커맨드라인 인자 파싱 에러 메시지 사용자 친화적으로 개선

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("인자 파싱에 문제가 발생했습니다: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {:?}", config.query);
     println!("In file {:?}", config.filename);
@@ -20,14 +24,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("인자가 충분하지 않습니다.");
+            return Err("인자가 충분하지 않습니다.");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
