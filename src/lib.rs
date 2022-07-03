c417b08cba730ba79c82626eb6e5819d1c670503
rulewrite <rulewrite@gmail.com>
2022-07-04 01:08:19 +0900

    feat: Config의 필드 값에 따라 검색과 대소문자 구분하지 않는 검색 로직 수행

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,13 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let contents = fs::read_to_string(config.filename)?;
 
-  for line in search(&config.query, &contents) {
+  let results = if config.ignore_case {
+    search_case_insensitive(&config.query, &contents)
+  } else {
+    search(&config.query, &contents)
+  };
+
+  for line in results {
     println!("{}", line);
   }
 
