bd9e29a45ca5da6821d026360eadf1730ccc052c
rulewrite <rulewrite@gmail.com>
2022-07-04 02:17:55 +0900

    fix: 에러 메시지는 표준 에러 스트림으로 출력되도록

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("인자 파싱에 문제가 발생했습니다: {}", err);
+        eprintln!("인자 파싱에 문제가 발생했습니다: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("애플리케이션 에러: {}", e);
+        eprintln!("애플리케이션 에러: {}", e);
         process::exit(1);
     }
 }
