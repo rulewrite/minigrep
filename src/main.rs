caeedd3eadecd8b23cb64a50b1661e20cf2d1d4b
rulewrite <rulewrite@gmail.com>
2022-07-14 23:16:04 +0900

    feat: Cargo 라이센스 및 설명 추가

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+use minigrep_rulewrite::Config;
 
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
@@ -9,7 +9,7 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = minigrep_rulewrite::run(config) {
         eprintln!("애플리케이션 에러: {}", e);
         process::exit(1);
     }
